776e10aee14edb095f5ef10f786bc68ff6385a31
Lepocka <mykola.barannyk@gmail.com>
2025-09-18 22:53:17 +0300

    add input number validation

--- Calculator.py
+++ Calculator.py
@@ -1,5 +1,14 @@
 first_num = input('Input first number: ')
+
+while not (str.isdigit(first_num)):
+    print('This is not a number')
+    first_num = input('Input first number again: ')
+
 second_number = input('Input second number: ')
+
+while not (str.isdigit(second_number)):
+    print('This is not a number')
+    second_number = input('Input second number again: ')
 
 result = int(first_num) + int(second_number)
 

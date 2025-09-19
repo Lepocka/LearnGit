5c1968196824ba0f486d1f7be66856bf6b37644a
Lepocka <mykola.barannyk@gmail.com>
2025-09-19 13:18:09 +0300

    added 3 new operations

--- Calculator.py
+++ Calculator.py
@@ -9,10 +9,31 @@ def number_validation(input_number):
     number = input_number
 
     while not (is_number(number)):
-        print('This is not a number')
-        number = input('Input number again: ')
+        print('Incorrect input')
+        number = input('Input again: ')
 
-    return number 
+    return number
+
+def operator_instruction():
+    print('0: +')
+    print('1: -')
+    print('2: *')
+    print('3: /')
+
+def calculating(operator, first_num, second_num):
+    while True:
+        if float(operator) == 0:
+            return float(first_num) + float(second_num)
+        elif float(operator) == 1:
+            return float(first_num) - float(second_num)
+        elif float(operator) == 2:
+            return float(first_num) * float(second_num)
+        elif float(operator) == 3:
+            return float(first_num) / float(second_num)
+        else:
+            print('Incorrect input')
+            operator_instruction()
+            operator = input('Choose operator again: ')
 
 first_number = input('Input first number: ')
 
@@ -22,6 +43,12 @@ second_number = input('Input second number: ')
 
 second_number = number_validation(second_number)
 
-result = float(first_number) + float(second_number)
+operator_instruction()
+
+input_operator = input("Choose operator: ")
+
+input_operator = number_validation(input_operator)
+
+result = calculating(input_operator, first_number, second_number)
 
 print(f'Result: {result}')
